108dc9ad6b0470eb87cc39052a6e246c8560e48f
DrDog <donannie@earthlink.net>
2025-07-20 18:16:58 -0400

    Update __init__.py

--- components/wifi_protocol/__init__.py
+++ components/wifi_protocol/__init__.py
@@ -4,8 +4,10 @@ import esphome.config_validation as cv
 wifi_protocol_ns = cg.esphome_ns.namespace("wifi_protocol")
 WiFiProtocolComponent = wifi_protocol_ns.class_("WiFiProtocolComponent", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({}).extend({})
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(WiFiProtocolComponent),
+})
 
 async def to_code(config):
-    var = cg.new_Pvariable("wifi_protocol_component", WiFiProtocolComponent())
+    var = cg.new_Pvariable(config[cg.CONF_ID])
     await cg.register_component(var, config)
