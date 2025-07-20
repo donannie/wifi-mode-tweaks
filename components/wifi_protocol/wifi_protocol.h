b7c539537306852424d69b5904f94e3751cfa766
DrDog <donannie@earthlink.net>
2025-07-20 18:00:55 -0400

    Update wifi_protocol.h

--- components/wifi_protocol/wifi_protocol.h
+++ components/wifi_protocol/wifi_protocol.h
@@ -1,1 +1,14 @@
+#pragma once
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace wifi_protocol {
+
+class WiFiProtocolComponent : public Component {
+ public:
+  void setup() override;
+};
+
+}  // namespace wifi_protocol
+}  // namespace esphome
 
