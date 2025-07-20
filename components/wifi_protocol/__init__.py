f81a2e2e69cf2a5a603a8cd7a3c0b11317a81f0b
DrDog <donannie@earthlink.net>
2025-07-20 18:11:53 -0400

    Update __init__.py

--- components/wifi_protocol/__init__.py
+++ components/wifi_protocol/__init__.py
@@ -1,1 +1,11 @@
-# Required for ESPHome to recognize this as a valid component
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+wifi_protocol_ns = cg.esphome_ns.namespace("wifi_protocol")
+WiFiProtocolComponent = wifi_protocol_ns.class_("WiFiProtocolComponent", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({}).extend({})
+
+async def to_code(config):
+    var = cg.new_Pvariable("wifi_protocol_component", WiFiProtocolComponent())
+    await cg.register_component(var, config)
