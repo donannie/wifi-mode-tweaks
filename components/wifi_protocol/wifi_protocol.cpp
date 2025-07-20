1056990108826d4c65665d49841fa9b270259f91
DrDog <donannie@earthlink.net>
2025-07-20 16:49:43 -0400

    Update wifi_protocol.cpp

--- components/wifi_protocol/wifi_protocol.cpp
+++ components/wifi_protocol/wifi_protocol.cpp
@@ -1,1 +1,17 @@
-//
+#include "wifi_protocol.h"
+#include "esphome/core/log.h"
+#include "esp_wifi.h"
+
+namespace esphome {
+namespace wifi_protocol {
+
+static const char *TAG = "wifi_protocol";
+
+void WiFiProtocolComponent::setup() {
+  ESP_LOGD(TAG, "Setting Wi-Fi to b/g only");
+  esp_wifi_set_protocol(WIFI_IF_STA, WIFI_PROTOCOL_11B | WIFI_PROTOCOL_11G);
+}
+
+}  // namespace wifi_protocol
+}  // namespace esphome
+
