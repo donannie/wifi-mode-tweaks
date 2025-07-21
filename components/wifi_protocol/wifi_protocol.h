68194e98f9ecaade10a1057ad05162b2e7c94ee5
DrDog <donannie@earthlink.net>
2025-07-20 20:32:07 -0400

    Update wifi_protocol.h

--- components/wifi_protocol/wifi_protocol.h
+++ components/wifi_protocol/wifi_protocol.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "esphome/core/component.h"
+#include "esphome/components/switch/switch.h"
 
 namespace esphome {
 namespace wifi_protocol {
 
-class WiFiProtocolComponent : public Component {
+class WiFiProtocolComponent : public esphome::Component, public esphome::switch_::Switch {
  public:
   void setup() override;
+  void write_state(bool state) override;
 };
 
 }  // namespace wifi_protocol
 }  // namespace esphome
-
