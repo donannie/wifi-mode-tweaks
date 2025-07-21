112ba98e28361c39ec14ea02ed2e7ab55384aba5
DrDog <donannie@earthlink.net>
2025-07-20 20:36:52 -0400

    Update __init__.py

--- components/wifi_protocol/__init__.py
+++ components/wifi_protocol/__init__.py
@@ -1,9 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+from esphome.components import switch  # ✅ Import switch module properly
 
 wifi_protocol_ns = cg.esphome_ns.namespace("wifi_protocol")
-WiFiProtocolComponent = wifi_protocol_ns.class_("WiFiProtocolComponent", cg.Component, cg.Switch)
+WiFiProtocolComponent = wifi_protocol_ns.class_(
+    "WiFiProtocolComponent", cg.Component, switch.Switch  # ✅ Extend Switch correctly
+)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(WiFiProtocolComponent),
@@ -12,4 +15,4 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await cg.register_switch(var, config)
+    await switch.register_switch(var, config)  # ✅ Register as a switch
