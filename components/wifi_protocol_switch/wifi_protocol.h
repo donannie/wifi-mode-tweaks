44a7c4afdd5d943f29019fef0a59d82fcffb5803
DrDog <donannie@earthlink.net>
2025-07-20 21:47:48 -0400

    Update wifi_protocol.h

--- components/wifi_protocol_switch/wifi_protocol.h
+++ components/wifi_protocol_switch/wifi_protocol.h
@@ -3,12 +3,12 @@
 #include "esphome/components/switch/switch.h"
 
 namespace esphome {
-namespace wifi_protocol_switch {
+namespace wifi_protocol_switch {  // ✅ Match your renamed component folder and Python namespace
 
 class WiFiProtocolComponent : public esphome::Component, public esphome::switch_::Switch {
  public:
-  void setup() override;
-  void write_state(bool state) override;
+  void setup() override;  // Called when ESPHome boots
+  void write_state(bool state) override;  // Called when switch is toggled
 };
 
 }  // namespace wifi_protocol_switch
